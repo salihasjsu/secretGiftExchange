eb99ea9bde00d30ad4695be0f032055774574fff
saliha mehboob <saliha.mehboob@sjsu.edu>
2021-06-03 19:45:18 -0700

    First commit

---
+++ src/main/java/com/secretGiftExchange/SecretGiftExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.secretGiftExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretGiftExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretGiftExchangeApplication.class, args);
+	}
+
+}
